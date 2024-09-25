dc8db07970c1abecc3d39bcf147f7601732d44cd
CTeufel <cteufel@ethz.ch>
2024-09-24 22:37:48 -0400

    stuff

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import './App.css';
 import Navbar from './nav.js' ;
 
 function App() {
-  Navbar();
-
   return (
     <div className="App">
-      <h1>My First React App</h1>
+      <Navbar/>
     </div>
   );
 }
